654b31d826e137a0d48821b61faf757bdb0209cc
amanjunath@minddoc.de <amanjunath@minddoc.de>
2018-11-20 13:14:54 +0100

    Add firebase and bootstrap dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,22 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import BootstrapVue from 'bootstrap-vue';
+import firebase from 'firebase';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+  databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SEND_ID',
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
